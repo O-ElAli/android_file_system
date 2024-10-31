3784fbca401cde3706696d95e70f10c47923605a
EragonBrisingr <geometrydasher26@gmail.com>
2024-10-31 22:38:34 +0300

    i think this thing works for now

--- android/app/src/main/java/com/file/MainApplication.kt
+++ android/app/src/main/java/com/file/MainApplication.kt
@@ -11,15 +11,17 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+import com.alpha0010.fs.FileAccessPackage;
 
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
+            PackageList(this).packages.toMutableList().apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
+              add(FileAccessPackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
